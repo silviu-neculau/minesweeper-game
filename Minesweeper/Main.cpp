2baac88dd773238e5498a5757ef930810816c841
silviu-neculau <silviu.mihai.neculau@gmail.com>
2018-08-11 16:06:54 +0300

    Initial commit

---
+++ Minesweeper/Main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+
+	// Minesweeper(dimensiune, numarMine, randomNumberGenerator)
+	// Minesweeper(dimensiune, numarMine)
+	// Minesweeper(dimensiune)
+	// constructor chaining 
+
+	// documentatie pe metode (unde e cazul)..
+	// comis pe git pe un repository -- minesweeper-game
+
+	Minesweeper m = new Minesweeper(8, 10);
+
+	while (m.isOver()) {
+		m.print(cout);
+		try {
+			m.makeMove(int rowNumber, int columnNumber);
+		}
+		catch (Exception e) {
+			//.. tratezi eroarea
+			//.. iei decizii daca continui sau opresti jocul
+		}
+	}
+
+	return 0;
+}
